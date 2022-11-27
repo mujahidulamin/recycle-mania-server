0541eed6419d08be70a509c2b361fa6c1e6ce800
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-11-27 23:39:47 +0600

    single-product-delete-api

--- index.js
+++ index.js
@@ -191,6 +191,22 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/products', async(req, res) => {
+            const query = {}
+            const products = await productsCollection.find(query).toArray();
+            res.send(products)
+        })
+
+        //delete a single product form seller dashboard api
+
+        app.delete('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(filter)
+            res.send(result);
+        })
+
+
 
 
 
